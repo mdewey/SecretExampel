1e84f6e69477361933bfb1696b6ad0e36c2b5789
Mark Dewey <mark.t.dewey@gmail.com>
2019-12-18 14:45:23 -0500

    first!

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.Extensions.Configuration;
+
+namespace SecretExampel.Models
+{
+    public partial class DatabaseContext : DbContext
+    {
+
+        public string ConnectionString { get; set; }
+
+        public DatabaseContext(IConfiguration configuration)
+        {
+            this.ConnectionString = configuration["ConnectionString"];
+            Console.WriteLine(this.ConnectionString);
+        }
+        private string ConvertPostConnectionToConnectionString(string connection)
+        {
+            var _connection = connection.Replace("postgres://", String.Empty);
+            var output = Regex.Split(_connection, ":|@|/");
+            return $"server={output[2]};database={output[4]};User Id={output[0]}; password={output[1]}; port={output[3]}";
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var envConn = Environment.GetEnvironmentVariable("DATABASE_URL");
+                var conn = this.ConnectionString;
+                if (envConn != null)
+                {
+                    conn = ConvertPostConnectionToConnectionString(envConn);
+                }
+                optionsBuilder.UseNpgsql(conn);
+            }
+        }
+
+
+
+    }
+}
